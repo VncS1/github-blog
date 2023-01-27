819d0130c9c041a5dba09c6483be0a3b50802290
Vinicius <62514933+VncS1@users.noreply.github.com>
2023-01-26 23:39:33 -0300

    Update index.tsx

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,7 @@
-import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { LayoutContainer } from "./styles";
+
+import { Outlet } from "react-router-dom";
 
 export function DefaultLayout() {
     return (
